9bb1b6e630e215703bed17236a9a7292b01d0707
Lucas Elisei <lucas.elisei@heig-vd.ch>
2017-06-11 17:09:00 +0200

    Adapted script for previous commit

--- scripts/step06.sh
+++ scripts/step06.sh
@@ -5,21 +5,21 @@
 docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
 
-docker rmi truanisei/static_http
+docker rmi truanisei/ajax_http
 docker rmi truanisei/dynamic_http
 docker rmi truanisei/traefik
 docker rmi $(docker images -q -f dangling=true)
 
-docker build -t truanisei/static_http $docker_dir/static_http/
+docker build -t truanisei/ajax_http $docker_dir/ajax_http/
 docker build -t truanisei/dynamic_http $docker_dir/dynamic_http/
 docker build -t truanisei/traefik $docker_dir/traefik/
 
 # Need to bind docker socket for Traefik.
-docker run -d -p 8080:80 -v /var/run/docker.sock:/var/run/docker.sock truanisei/traefik
+docker run -d -p 8080:80 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock truanisei/traefik
 
 for i in $(seq 1 $(shuf -i1-10 -n1))
 do
-    docker run -d truanisei/static_http
+    docker run -d truanisei/ajax_http
 done
 
 for i in $(seq 1 $(shuf -i1-10 -n1))
